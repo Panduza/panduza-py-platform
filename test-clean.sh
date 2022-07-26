55ac54d6821c8830920a9f6195f4b60a118c4f3f
XRodriguez <xavier.rodriguezalvarez@gmail.com>
2022-07-26 16:59:16 +0200

    fix test

--- test-clean.sh
+++
@@ -1,6 +0,0 @@
-
-
-rm -rfv test_reports/coverage
-rm -rfv test_reports/platform_log.txt
-rm -rfv test_reports/test.html
-rm -rfv .coverage
--- test-reports.sh
+++
@@ -1,9 +0,0 @@
-
-#
-behave -f html -o test_reports/test.html
-
-#
-coverage report
-
-#
-coverage html -d test_reports/coverage
