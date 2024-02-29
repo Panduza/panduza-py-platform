25de43cf5848c9386a8626e5451a62dac9cbc0be
Xavier RODRIGUEZ <xavier.rodriguezalvarez@gmail.com>
2024-02-29 14:04:31 +0100

    adapt runnig scripts

--- scripts/go.sh
+++ scripts/go.sh
@@ -1,13 +1,28 @@
-PYTHON_BIN=python3.11
-PYTHON_VENV_PATH=/opt/panduza/venv
+# Binairy install on your system
+# NATIVE_PYTHON_BIN=python3.11
+NATIVE_PYTHON_BIN=python
 
+# ---
+PYTHON_VENV_PATH=/opt/panduza/venv
+PYTHON_VENV_BIN=$PYTHON_VENV_PATH/bin/python
+PYTHON_VENV_PIP=$PYTHON_VENV_PATH/bin/pip3
+PYTHON_VENV_PZA_MAIN=$PYTHON_VENV_PATH/lib/python3.11/site-packages/panduza_platform/__main__.py
+# If windows system
+if [[ "$OSTYPE" == "cygwin" || "$OSTYPE" == "msys" ]]; then
+    PYTHON_VENV_PATH=~/panduza/venv
+    PYTHON_VENV_BIN=$PYTHON_VENV_PATH/Scripts/python
+    PYTHON_VENV_PIP=$PYTHON_VENV_PATH/Scripts/pip3
+    PYTHON_VENV_PZA_MAIN=$PYTHON_VENV_PATH/Lib/site-packages/panduza_platform/__main__.py
+fi
+
+# 
 script_dir=$(dirname $0)
 script_dir=$(readlink -f $script_dir)
 echo "Script directory: $script_dir"
 
 # 
-${PYTHON_VENV_PATH}/bin/pip3 install ${script_dir}/..
+${PYTHON_VENV_PIP} install ${script_dir}/..
 
 # 
-${PYTHON_VENV_PATH}/bin/${PYTHON_BIN}  ${PYTHON_VENV_PATH}/lib/python3.11/site-packages/panduza_platform/__main__.py
+${PYTHON_VENV_BIN}  ${PYTHON_VENV_PZA_MAIN}
 
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,15 @@
-PYTHON_BIN=python3.11
+# Binairy install on your system
+# NATIVE_PYTHON_BIN=python3.11
+NATIVE_PYTHON_BIN=python
+
+# ---
 PYTHON_VENV_PATH=/opt/panduza/venv
+PYTHON_VENV_PIP=$PYTHON_VENV_PATH/bin/pip3
+# If windows system
+if [[ "$OSTYPE" == "cygwin" || "$OSTYPE" == "msys" ]]; then
+    PYTHON_VENV_PATH=~/panduza/venv
+    PYTHON_VENV_PIP=$PYTHON_VENV_PATH/Scripts/pip3
+fi
 
 
 script_dir=$(dirname $0)
@@ -10,10 +20,10 @@ echo "Script directory: $script_dir"
 mkdir -p ${PYTHON_VENV_PATH}
 
 # Create venv
-${PYTHON_BIN} -m venv ${PYTHON_VENV_PATH}
+${NATIVE_PYTHON_BIN} -m venv ${PYTHON_VENV_PATH}
 
 # 
-${PYTHON_VENV_PATH}/bin/pip3 install -r ${script_dir}/../requirements.txt
+${PYTHON_VENV_PIP} install -r ${script_dir}/../requirements.txt
 
 # 
-${PYTHON_VENV_PATH}/bin/pip3 install ${script_dir}/..
+${PYTHON_VENV_PIP} install ${script_dir}/..
